efb28df52ed2f18265ea4614d802d0c8bf81a569
loving-cat <106512832+loving-cat@users.noreply.github.com>
2022-06-20 17:52:52 +0800

    Add files via upload

--- bit数组.c
+++ bit数组.c
@@ -61,3 +61,51 @@
 //	}
 //	return 0;
 //}
+
+
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int i = 0;
+//	for ( i = 0; i < sz; i++)
+//	{
+//		printf("&arr[%d] = %p\n", i, &arr[i]);
+//	}
+//	return 0;
+//}
+
+//¶þÎ¬Êý×é
+//int main()
+//{
+//	int arr[3][4] = { {1,2,3},{4,5} };
+//	//char ch[5][6];
+//	int i = 0;
+//	for ( i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for ( j = 0; j < 4; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
+int main()
+{
+	int arr[3][4] = { {1,2,3},{4,5} };
+	//char ch[5][6];
+	int i = 0;
+	for (i = 0; i < 3; i++)
+	{
+		int j = 0;
+		for (j = 0; j < 4; j++)
+		{
+			printf("&arr[%d][%d] = %p\n ",i,j,& arr[i][j]);
+		}
+	}
+
+	return 0;
+}
