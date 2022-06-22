05e823e9e6838c64496599940c764a628675a756
loving-cat <106512832+loving-cat@users.noreply.github.com>
2022-06-22 20:13:26 +0800

    Add files via upload

--- bit数组.c
+++ bit数组.c
@@ -93,19 +93,61 @@
 //
 //	return 0;
 //}
-int main()
-{
-	int arr[3][4] = { {1,2,3},{4,5} };
-	//char ch[5][6];
-	int i = 0;
-	for (i = 0; i < 3; i++)
-	{
-		int j = 0;
-		for (j = 0; j < 4; j++)
-		{
-			printf("&arr[%d][%d] = %p\n ",i,j,& arr[i][j]);
-		}
-	}
+//int main()
+//{
+//	int arr[3][4] = { {1,2,3},{4,5} };
+//	//char ch[5][6];
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 4; j++)
+//		{
+//			printf("&arr[%d][%d] = %p\n ",i,j,& arr[i][j]);
+//		}
+//	}
+//
+//	return 0;
+//}
 
-	return 0;
-}
+//int main()
+//{
+//	int arr[3][4];
+//	//char ch[5][6];
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 4; j++)
+//		{
+//			printf("&arr[%d][%d] = %p\n ", i, j, &arr[i][j]);
+//		}
+//	}
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[3][4] = {0};
+//	//char ch[5][6];
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 4; j++)
+//		{
+//			arr[i][j] = i * 4 + j;
+//		}
+//	}
+//	for ( i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//	}
+//
+//	return 0;
+//}
+//int main()
+//{
+//
+//	return 0;
+//}
---
+++ 冒泡排序.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+void bubble_sort(int arr[])
+{
+	1000000000001111
+}
+int main()
+{
+	int arr[] = { 9,8,7,6,5,4,3,2,1, };
+	//对arr进行排序，排成升序
+	bubble_sort(arr);//冒泡排序函数
+	return 0;
+}
