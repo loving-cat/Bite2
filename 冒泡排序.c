093bd5a42925ced9c814e762b11cf8e9e645a5de
loving-cat <106512832+loving-cat@users.noreply.github.com>
2022-06-23 21:02:55 +0800

    Add files via upload

--- 冒泡排序.c
+++ 冒泡排序.c
@@ -1,13 +1,72 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//void bubble_sort(int arr[],int sz)
+//{
+//	int flag = 1;//假设这一趟要排序的数据已经有序
+//	//确定冒泡排序的趟数
+//	int i = 0;
+//	for (i = 0; i < sz-1; i++)
+//	{
+//		//每一趟冒泡排序
+//		int j = 0;
+//		for ( j = 0; j < sz-1-i; j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				int tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//				flag = 0;//本趟排序的数据其实不完全有序
+//			}
+//		}
+//		if (flag == 1)
+//		{
+//			break;
+//		}
+//	}
+//}
+//int main()
+//{
+//	int arr[] = { 9,8,7,6,5,4,3,2,1,0};
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	//对arr进行排序，排成升序
+//	//arr 是数组，我们对数组arr进行传参，实际上传递过去的是数组arr首元素的地址
+//	bubble_sort(arr,sz);//冒泡排序函数
+//	for ( i = 0; i < sz; i++)
+//	{
+//		printf("%d", i);
+//	}
+//	return 0;
+//}
 
-void bubble_sort(int arr[])
-{
-	1000000000001111
-}
+//int main()
+//{
+//	if (1)
+//		break; //err
+//	return 0;
+//}
+
 int main()
 {
-	int arr[] = { 9,8,7,6,5,4,3,2,1, };
-	//对arr进行排序，排成升序
-	bubble_sort(arr);//冒泡排序函数
+	////数组名就是数组首元素地址
+	int arr[] = { 1,2,3,4,5,6,7 };
+	printf("%p\n", arr);
+	printf("%p\n", arr+1);
+
+	printf("%p\n", &arr[0]);
+	printf("%p\n", &arr[0]+1);
+
+	printf("%p\n", &arr);//取出的是数组的地址
+	printf("%p\n", &arr+1);
+
+	//int sz = sizeof(arr) / sizeof(arr[0]);
+	//数组名就是首元素地址，但有例外
+	// 1.sizeof(数组名) - 数组名表示整个数组，
+	// sizeof(数组名)计算的是整个数组的大小，单位是字节
+	// 2.&数组名，数组名代表整个数组，&数组名，取出的是整个数组的地址
+	//printf("%p\n", arr);
+	//printf("%p\n", &arr[0]);
+	//printf("%d\n", *arr);//1
 	return 0;
 }
