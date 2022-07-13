3623c4bd532c146b4beb036580934a5dfd3b4455
loving-cat <106512832+loving-cat@users.noreply.github.com>
2022-07-13 19:51:24 +0800

    Add files via upload

---
+++ Test22.java
@@ -0,0 +1,17 @@
+/**
+  whileÑ­»·
+  2.¼ÆËã10µÄ½×³Ë
+*/
+import java.util.Scanner;
+public class Test22{
+	public static void main(String[] args){
+		
+		int i = 1;
+		int num = 1;
+		while(i<=10){
+			num *= i;
+			i++;
+		}
+		System.out.println(num);
+	}
+}
